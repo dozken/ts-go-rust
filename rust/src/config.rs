fe3d9c972d94880f2b10b293984ba7ba7c2dea9c
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-11-13 22:15:27 +0800

    rust config test

--- rust/src/config.rs
+++ rust/src/config.rs
@@ -27,7 +27,7 @@ impl TryFrom<Opts> for Config {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Operation {
     Print(Option<String>),
     Add((String, String)),
@@ -99,4 +99,70 @@ fn get_pwd(pwd: Option<PathBuf>) -> Result<PathBuf> {
     }
 
     return Ok(std::env::current_dir().context("unable to find $HOME")?);
+}
+
+#[cfg(test)]
+mod test {
+    use super::Config;
+    use crate::{config::Operation, opts::Opts};
+    use anyhow::Result;
+
+    #[test]
+    fn test_print_all() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![],
+            pwd: None,
+            config: None,
+        }
+        .try_into()?;
+
+        assert_eq!(opts.operation, Operation::Print(None));
+        return Ok(());
+    }
+
+    #[test]
+    fn test_print_key() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![String::from("key")],
+            pwd: None,
+            config: None,
+        }
+        .try_into()?;
+
+        assert_eq!(opts.operation, Operation::Print(Some(String::from("key"))));
+        return Ok(());
+    }
+
+    #[test]
+    fn test_add_key_value() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![
+                String::from("add"),
+                String::from("key"),
+                String::from("value"),
+            ],
+            pwd: None,
+            config: None,
+        }
+        .try_into()?;
+
+        assert_eq!(
+            opts.operation,
+            Operation::Add((String::from("key"), String::from("value")))
+        );
+        return Ok(());
+    }
+
+    #[test]
+    fn test_rm_key_value() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![String::from("rm"), String::from("key")],
+            pwd: None,
+            config: None,
+        }
+        .try_into()?;
+
+        assert_eq!(opts.operation, Operation::Remove(String::from("key")));
+        return Ok(());
+    }
 }
